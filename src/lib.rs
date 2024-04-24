d632c5ce832e6b406fe165fdfd8454caa438c99e
Takeru Ohta <phjgt308@gmail.com>
2024-04-24 14:33:35 +0900

    Add list command

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use orfail::OrFail;
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug)]
+pub struct LogFilePathIterator {
+    stack: Vec<PathBuf>,
+    visited: HashSet<PathBuf>,
+}
+
+impl LogFilePathIterator {
+    pub fn new<P: AsRef<Path>>(root_dir: P) -> Self {
+        Self {
+            stack: vec![root_dir.as_ref().to_path_buf()],
+            visited: HashSet::new(),
+        }
+    }
+
+    fn next_item(&mut self) -> orfail::Result<Option<(LogFileKind, PathBuf)>> {
+        while let Some(path) = self.stack.pop() {
+            if self.visited.contains(&path) {
+                continue;
+            }
+            self.visited.insert(path.clone());
+
+            if path.is_dir() {
+                for entry in std::fs::read_dir(path).or_fail()? {
+                    let child_path = entry.or_fail()?.path();
+                    if let Ok(child_path) = child_path.canonicalize() {
+                        self.stack.push(child_path);
+                    }
+                }
+                continue;
+            }
+
+            let Some(kind) = LogFileKind::from_path(&path) else {
+                continue;
+            };
+
+            return Ok(Some((kind, path)));
+        }
+        Ok(None)
+    }
+}
+
+impl Iterator for LogFilePathIterator {
+    type Item = orfail::Result<(LogFileKind, PathBuf)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_item().or_fail().transpose()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum LogFileKind {
+    Sora,
+    Cluster,
+    Debug,
+    Internal,
+    Api,
+    Crash,
+    Signaling,
+    Connection,
+    EventWebhook,
+    SessionWebhook,
+}
+
+impl LogFileKind {
+    fn from_path(path: &PathBuf) -> Option<Self> {
+        let Some(name) = path.file_name() else {
+            return None;
+        };
+        let Some(name) = name.to_str() else {
+            return None;
+        };
+
+        match name {
+            "sora.jsonl" => Some(Self::Sora),
+            "cluster.jsonl" => Some(Self::Cluster),
+            "debug.jsonl" => Some(Self::Debug),
+            "internal.jsonl" => Some(Self::Internal),
+            "api.jsonl" => Some(Self::Api),
+            "crash.log" => Some(Self::Crash),
+            "signaling.jsonl" => Some(Self::Signaling),
+            "connection.jsonl" => Some(Self::Connection),
+            "event_webhook.jsonl" => Some(Self::EventWebhook),
+            "session_webhook.jsonl" => Some(Self::SessionWebhook),
+            _ => None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use clap::Parser;
+use orfail::OrFail;
+use soralog::LogFilePathIterator;
+use std::io::Write;
+use std::path::PathBuf;
 
 #[derive(Parser)]
-struct Args {}
+enum Args {
+    List {
+        #[clap(long, default_value = ".")]
+        root: PathBuf,
+    },
+}
 
 fn main() -> orfail::Result<()> {
-    let _args = Args::parse();
+    let args = Args::parse();
+    match args {
+        Args::List { root } => {
+            let files = LogFilePathIterator::new(&root)
+                .map(|item| item.map(|(_, path)| path))
+                .collect::<orfail::Result<Vec<_>>>()
+                .or_fail()?;
+            output_json(files).or_fail()?;
+        }
+    }
+    Ok(())
+}
+
+fn output_json<T: serde::Serialize>(value: T) -> orfail::Result<()> {
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+    serde_json::to_writer_pretty(&mut stdout, &value).or_fail()?;
+    writeln!(stdout).or_fail()?;
     Ok(())
 }
