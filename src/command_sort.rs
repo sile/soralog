dfde1b32ff2ef75d04980b5e8272917917d1dbdf
Takeru Ohta <phjgt308@gmail.com>
2024-04-28 22:25:21 +0900

    Update sort command

--- src/command_sort.rs
+++ src/command_sort.rs
@@ -1,27 +1,24 @@
 use crate::{jsonl, message::Message};
 use orfail::OrFail;
-use std::cmp::Ordering;
 
 #[derive(Debug, clap::Args)]
-pub struct SortCommand {
-    #[clap(default_value = "timestamp")]
-    pub keys: Vec<String>,
-}
+pub struct SortCommand {}
 
 impl SortCommand {
     pub fn run(&self) -> orfail::Result<()> {
         let mut messages = jsonl::input_items::<Message>()
             .collect::<orfail::Result<Vec<_>>>()
             .or_fail()?;
-        messages.sort_by(|a, b| {
-            self.keys
-                .iter()
-                .copied()
-                .map(|key| a.get_value(key).cmp(&b.get_value(key)))
-                .find(|order| !order.is_eq())
-                .unwrap_or(Ordering::Equal)
-        });
+        messages.sort_by(|a, b| get_timestamp(a).cmp(&get_timestamp(b)));
         jsonl::output_items(messages.into_iter().map(Ok)).or_fail()?;
         Ok(())
+    }
+}
+
+fn get_timestamp(m: &Message) -> Option<&str> {
+    if let Some(serde_json::Value::String(t)) = m.get_value("timestamp") {
+        Some(t)
+    } else {
+        None
     }
 }
